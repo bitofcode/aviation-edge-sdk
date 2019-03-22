46f509a08e0be4ffffe5340c941ca4ec6ad316f3
Wassim Akachi <wassim@wassimakachi.com>
2019-03-23 00:57:42 +0100

    Extend the QueryParameterName Enum

--- src/main/java/com/bitofcode/oss/sdk/com/aviationedge/resources/QueryParameterName.java
+++ src/main/java/com/bitofcode/oss/sdk/com/aviationedge/resources/QueryParameterName.java
@@ -2,9 +2,25 @@ package com.bitofcode.oss.sdk.com.aviationedge.resources;
 
 public enum QueryParameterName {
   API_KEY("key"),
+
   AIRLINE_IATA_CODE("codeIataAirline"),
+  AIRLINE_COUNTRY_ISO_2_CODE("codeIso2Country"),
+
+  AIRCRAFT_IATA_CODE("codeIataAircraft"),
+
+  AIRPLANE_NUMBER_REGISTRATION("numberRegistration"),
+  AIRPLANE_HEX_ICAO("hexIcaoAirplane"),
+  AIRPLANE_IATA_CODE("codeIataAirline"),
+
   AIRPORT_IATA_CODE("codeIataAirport"),
+  AIRPORT_COUNTRY_ISO_2_CODE("codeIso2Country"),
+
+  CITY_IATA_CODE("codeIataCity"),
+  CITY_COUNTRY_ISO_2_CODE("codeIso2Country"),
+
   COUNTRY_ISO_2_CODE("codeIso2Country"),
+  COUNTRY_NAME("nameCountry"),
+
   FLIGHT_DEP_IATA("depIata"),
   FLIGHT_DEP_ICAO("depIcao"),
   FLIGHT_ARR_IATA("arrIata"),
@@ -19,6 +35,8 @@ public enum QueryParameterName {
   FLIGHT_NUMBER("flightNum"),
   FLIGHT_STATUS("status"),
   FLIGHT_LIMIT("limit"),
+
+  TAX_IATA_CODE("codeIataTax"),
   ;
 
   private final String name;
--- src/test/java/com/bitofcode/oss/sdk/com/aviationedge/resources/QueryParameterNameTest.java
+++ src/test/java/com/bitofcode/oss/sdk/com/aviationedge/resources/QueryParameterNameTest.java
@@ -13,21 +13,40 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class QueryParameterNameTest {
 
+  public static final String CODE_ISO_2_COUNTRY = "codeIso2Country";
   private final Map<QueryParameterName, String> possibleParameters;
 
   QueryParameterNameTest() {
     Map<QueryParameterName, String> temPam = new HashMap<>();
     temPam.put(QueryParameterName.API_KEY, "key");
+
     temPam.put(QueryParameterName.AIRLINE_IATA_CODE, "codeIataAirline");
+    temPam.put(QueryParameterName.AIRLINE_COUNTRY_ISO_2_CODE, CODE_ISO_2_COUNTRY);
+
+    temPam.put(QueryParameterName.AIRCRAFT_IATA_CODE, "codeIataAircraft");
+
+    temPam.put(QueryParameterName.AIRPLANE_NUMBER_REGISTRATION, "numberRegistration");
+    temPam.put(QueryParameterName.AIRPLANE_HEX_ICAO, "hexIcaoAirplane");
+    temPam.put(QueryParameterName.AIRPLANE_IATA_CODE, "codeIataAirline");
+
+
     temPam.put(QueryParameterName.AIRPORT_IATA_CODE, "codeIataAirport");
-    temPam.put(QueryParameterName.COUNTRY_ISO_2_CODE, "codeIso2Country");
+    temPam.put(QueryParameterName.AIRPORT_COUNTRY_ISO_2_CODE, CODE_ISO_2_COUNTRY);
+
+
+    temPam.put(QueryParameterName.CITY_IATA_CODE, "codeIataCity");
+    temPam.put(QueryParameterName.CITY_COUNTRY_ISO_2_CODE, CODE_ISO_2_COUNTRY);
+
+    temPam.put(QueryParameterName.COUNTRY_ISO_2_CODE, CODE_ISO_2_COUNTRY);
+    temPam.put(QueryParameterName.COUNTRY_NAME, "nameCountry");
+
     temPam.put(QueryParameterName.FLIGHT_DEP_IATA, "depIata");
     temPam.put(QueryParameterName.FLIGHT_DEP_ICAO, "depIcao");
     temPam.put(QueryParameterName.FLIGHT_ARR_IATA, "arrIata");
     temPam.put(QueryParameterName.FLIGHT_ARR_ICAO, "arrIcao");
     temPam.put(QueryParameterName.FLIGHT_AIRCRAFT_ICAO, "aircraftIcao");
-    temPam.put(QueryParameterName.FLIGHT_AIRCRAFT_ICAO_24, "aircraftIcao24");
     temPam.put(QueryParameterName.FLIGHT_REG_NUMBER, "regNum");
+    temPam.put(QueryParameterName.FLIGHT_AIRCRAFT_ICAO_24, "aircraftIcao24");
     temPam.put(QueryParameterName.FLIGHT_AIRLINE_IATA, "airlineIata");
     temPam.put(QueryParameterName.FLIGHT_AIRLINE_ICAO, "airlineIcao");
     temPam.put(QueryParameterName.FLIGHT_IATA, "flightIata");
@@ -35,6 +54,8 @@ class QueryParameterNameTest {
     temPam.put(QueryParameterName.FLIGHT_NUMBER, "flightNum");
     temPam.put(QueryParameterName.FLIGHT_STATUS, "status");
     temPam.put(QueryParameterName.FLIGHT_LIMIT, "limit");
+
+    temPam.put(QueryParameterName.TAX_IATA_CODE, "codeIataTax");
 
     possibleParameters = Collections.unmodifiableMap(temPam);
   }
